5fca22532b28711e373d7da7f369f21697e3d21d
mr.piash.u@gmail.com <mr.piash.u@gmail.com>
2025-10-15 20:39:48 +0600

    app file setup and env file added

--- src/app.js
+++ src/app.js
@@ -1,70 +1,40 @@
 
+import express from 'express';
+import cors from 'cors';
+import { notfoundHandler, errorHandler } from './utilites/errorHandler.js';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
 
 
 
+app.use(express.json({
+    limit: '16kb'
+}));
 
+app.use(express.urlencoded({
+    extended: true,
+    limit: '16kb'
+}))
 
+app.use(express.static('public'));
 
+// enable CORS
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
 
 
 
 
+// 404 not found handler
+app.use(notfoundHandler);
 
+// error handler
+app.use(errorHandler);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const port = process.env.PORT || 3444;
+app.listen(port, () => {
+    console.log(`Server is running on port : ${port}`);
+});
